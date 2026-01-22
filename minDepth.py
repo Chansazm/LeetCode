6256957b808a11fa845db7442748f0e808e1f6ba
Chansa Chishimba <chansazm@yahoo.co.uk>
2026-01-22 14:58:18 +0000

    added minimum depth

---
+++ minDepth.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self,val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def minDepth(self,root):
+        if not root:
+            return 0
+        
+        if not root.left and root.right:
+            return 1 + self.minDepth(root.right)
+        if not root.right and root.left:
+            return 1 + self.minDepth(root.left)
+        
+        return 1 + min(self.minDepth(root.right),self.minDepth(root.left))
